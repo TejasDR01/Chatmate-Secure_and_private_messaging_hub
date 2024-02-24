113004bf4d42022c280ae97be3c23810147b1a6d
Tejas D R <tejasdr01@gmail.com>
2024-02-24 21:57:03 +0530

    changed baseURL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL:"https://chatmate-secure-and-private-messaging-hub.vercel.app/" });
+const API = axios.create({
+  baseURL: "https://chatmate-secure-and-private-messaging-hub.onrender.com",
+});
 
 API.interceptors.request.use((req) => {
-    const localdata = localStorage.getItem("profile");
-    if(localdata){
-        req.headers.authorization = `Bearer ${JSON.parse(localdata).token}`;
-    }
-    return req;
+  const localdata = localStorage.getItem("profile");
+  if (localdata) {
+    req.headers.authorization = `Bearer ${JSON.parse(localdata).token}`;
+  }
+  return req;
 });
 
 export const getChats = () => API.get("/chats");
