46fd99a11d5cf5e15e77b6c3d6736f3e11d9e96b
TejasDR01 <72020063+TejasDR01@users.noreply.github.com>
2024-02-24 20:06:06 +0530

    Updated baseURL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL:"http://localhost:5000" });
+const API = axios.create({ baseURL:"https://chatmate-secure-and-private-messaging-hub.vercel.app/" });
 
 API.interceptors.request.use((req) => {
     const localdata = localStorage.getItem("profile");
